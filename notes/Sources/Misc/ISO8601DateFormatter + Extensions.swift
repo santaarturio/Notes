fc77423f0f09788a4ff7880133b2b3bdcc9fc093
Artur Nikolaienko <artur.nikolaienko@sigma.software>
2021-09-27 17:42:06 +0300

    cached ISO8601DateFormatter

--- notes/Sources/Misc/ISO8601DateFormatter + Extensions.swift
+++ notes/Sources/Misc/ISO8601DateFormatter + Extensions.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-fileprivate let cachedFormatter = ISO8601DateFormatter()
-
 extension ISO8601DateFormatter {
-  static var cached: ISO8601DateFormatter { cachedFormatter }
+  static var cached: ISO8601DateFormatter = .init()
 }
